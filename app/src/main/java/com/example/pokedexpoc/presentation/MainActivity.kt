96836eb23f9591d1125a6f6963f731a69e284105
diegoSousa <diego.sousa@viavarejo.com.br>
2022-02-18 12:42:34 -0300

    Configures application modules

--- app/src/main/java/com/example/pokedexpoc/presentation/MainActivity.kt
+++ app/src/main/java/com/example/pokedexpoc/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.pokedexpoc
+package com.example.pokedexpoc.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pokedexpoc.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
