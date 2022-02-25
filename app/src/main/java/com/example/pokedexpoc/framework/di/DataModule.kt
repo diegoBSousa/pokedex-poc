0752388b09f2af763c8c273bc5cc024a19fa85b7
diegoSousa <diego.sousa@viavarejo.com.br>
2022-02-25 12:38:19 -0300

    Create HTTP Logging Interceptor

--- app/src/main/java/com/example/pokedexpoc/framework/di/DataModule.kt
+++ app/src/main/java/com/example/pokedexpoc/framework/di/DataModule.kt
@@ -1,10 +1,13 @@
 package com.example.pokedexpoc.framework.di
 
+import android.util.Log
 import com.example.core.data.repository.implementations.PokemonRepositoryImplementation
 import com.example.core.data.repository.interfaces.PokemonRepositoryInterface
 import com.example.core.data.services.PokemonService
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.core.module.Module
 import org.koin.dsl.module
 import retrofit2.Retrofit
@@ -12,6 +15,7 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 
 object DataModule {
     private const val BASE_URL = "https://pokeapi.co/api/v2/"
+    private const val OK_HTTP = "Ok Http"
 
     private fun pokemonsModule() : Module {
         return module {
@@ -24,21 +28,35 @@ object DataModule {
     private fun networkModule() : Module {
         return module {
             single {
-                createService(get())
+                createService(get(), get())
             }
 
             single {
                 Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
             }
+
+            single {
+                val interceptor = HttpLoggingInterceptor {
+                    Log.e(OK_HTTP, it )
+                }
+
+               interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+                OkHttpClient.Builder()
+                    .addInterceptor(interceptor)
+                    .build()
+            }
         }
     }
 
     private inline fun createService(
-        factory: Moshi
+        factory: Moshi,
+        client: OkHttpClient
     ) : PokemonService {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(MoshiConverterFactory.create(factory))
+            .client(client)
             .build()
             .create(PokemonService::class.java)
     }
