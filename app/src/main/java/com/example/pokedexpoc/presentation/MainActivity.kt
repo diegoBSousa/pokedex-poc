d61a13f7f949740d4294a3b38a2e136d30227dc8
diegoSousa <diego.sousa@viavarejo.com.br>
2022-03-01 14:03:38 -0300

    Configures navigation component

--- app/src/main/java/com/example/pokedexpoc/presentation/MainActivity.kt
+++ app/src/main/java/com/example/pokedexpoc/presentation/MainActivity.kt
@@ -2,17 +2,32 @@ package com.example.pokedexpoc.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 
 import com.example.pokedexpoc.R
+import com.example.pokedexpoc.databinding.ActivityMainBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     // Este 'by viewModel()' precisa ter o viewModel() do Koin carregado
     private val viewModel: MainViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(
+            setOf()
+        )
     }
 }
