e44e70db01f876e34f4926cd74b0fd3f36aab5d9
diegoSousa <diego.sousa@viavarejo.com.br>
2022-03-02 14:29:58 -0300

    Configures Pokemon Fragment

--- app/src/main/java/com/example/pokedexpoc/presentation/ui/pokemons/PokemonsFragment.kt
+++ app/src/main/java/com/example/pokedexpoc/presentation/ui/pokemons/PokemonsFragment.kt
@@ -5,15 +5,67 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.core.domain.model.Pokemon
 import com.example.pokedexpoc.R
+import com.example.pokedexpoc.databinding.FragmentPokemonsBinding
+
 
 class PokemonsFragment : Fragment() {
 
+    private var _binding: FragmentPokemonsBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    private val pokemonsAdapter = PokemonsAdapter()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentPokemonsBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initPokemonsAdapter()
+
+        /* *
+        * Todo("Remove data test")
+         */
+        pokemonsAdapter.submitList(
+            listOf(
+                Pokemon(1, "bulbasaur", "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"),
+                Pokemon(2, "ivysaur", "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png"),
+                Pokemon(3, "venusaur", "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png"),
+                Pokemon(4, "charmander", "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"),
+                Pokemon(5, "charmeleon", "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png"),
+                Pokemon(6, "charizard", "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png"),
+                Pokemon(7, "squirtle", "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png"),
+            )
+        )
+    }
+
+    private fun initPokemonsAdapter() {
+        with(binding.pokemonsRecycler) {
+            setHasFixedSize(true)
+            adapter = pokemonsAdapter
+        }
+    }
+    /*
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pokemons, container, false)
-    }
+    }*/
 }
